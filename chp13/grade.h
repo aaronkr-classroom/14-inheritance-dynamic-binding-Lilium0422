d8162f936496e142be1f39a350748506d6768365
Lilium0422 <a63789492@gmail.com>
2024-06-07 12:26:48 +0900

    chp13 실습 파일
    chp13 실습 파일 제출(::grade 오류 있음)

---
+++ chp13/grade.h
@@ -0,0 +1,15 @@
+#pragma once
+// grade.h
+#include <vector>
+
+#ifndef GUARD_grade_h
+#define GUARD_grade_h
+
+#include <list>
+
+using namespace std;
+
+double grade(double, double, double);
+double grade(double, double, vector<double>&);
+
+#endif
